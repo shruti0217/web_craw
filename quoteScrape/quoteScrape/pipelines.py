936eb8bb280b4af1ac5af0c50f468072a64f8680
Shruti <shrutigehlot2629@gmail.com>
2021-05-06 01:23:32 +0530

    Fixed errors in pipelines.py

--- quoteScrape/quoteScrape/pipelines.py
+++ quoteScrape/quoteScrape/pipelines.py
@@ -12,27 +12,26 @@ import sqlite3
 class QuotescrapePipeline:
     #as item pass through pipline , search for initialization method.
     def __init__(self):
-        #self.connect_DB()
-        #self.create_TB()
-        pass
+        self.connect_DB()
+        self.create_TB()
+        #pass
     
     
     def connect_DB(self):
-        #self.conn = sqlite3.connect('Quotes.db')
-        #self.connec = sqlite3.connect('Quotes.db')  #Throws error :/ why ?
-        #self.curr = self.conn.cursor()
+        self.conn = sqlite3.connect('Quotes.db')  #Throws error :/ why ?
+        self.curr = self.conn.cursor()
     def create_TB(self):
-        #self.curr.execute('''DROP table if EXISTS Quotes_table''')
-        #self.curr.execute('''create table Quotes_table(author text, quote  text,tag text)''')
+        self.curr.execute('''DROP table if EXISTS Quotes_table''')
+        self.curr.execute('''create table Quotes_table(author text, quote  text,tag text)''')
     
     def process_item(self, item, spider):
         #This method will be called automatically.    
         print('\n In the pipeline')
-        #self.store_DB(item)
+        self.store_DB(item)
         
         return item
         #it will still generate and store Through Export feeds.
 
     def store_DB(self,item):
-        #self.curr.execute("""insert into Quotes_table values(?,?,?)""",(item['author'],item['quote'],item['tags']))
-        #self.conn.commit()
+        self.curr.execute("""insert into Quotes_table values(?,?,?)""",(item['author'],item['quote'],item['tags']))
+        self.conn.commit()
